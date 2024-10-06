98aebf7647d111681b4cffcd2b044acedafc3a12
Pedro Afonso Paulina Ataide <150714429+Afonsope@users.noreply.github.com>
2024-10-06 17:18:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* Codigo feito pelo Afonsope */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                Console.WriteLine($"8 x {i} = {8 * i}");
+            }
+            Console.Read();
+        }
+    }
+}
