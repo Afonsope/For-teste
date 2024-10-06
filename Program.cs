2041164da491c0073257f3b1d5e857f8a1940949
Pedro Afonso Paulina Ataide <150714429+Afonsope@users.noreply.github.com>
2024-10-06 17:04:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* Codigo feito pelo Afonsope */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 30; i = i + 4)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
